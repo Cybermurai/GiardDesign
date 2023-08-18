8ffe8e2577b8ca758ed43857a25ae3d67550c03a
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-08-18 22:49:40 +0200

    Navigation dropdown, serach toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import * as bootstrap from 'bootstrap'
 import Navigation from "./global/navigation";
+import SearchInput from "./global/search";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const search = new SearchInput();
     const navigation = new Navigation();
 });
