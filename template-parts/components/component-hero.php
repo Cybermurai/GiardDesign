38c2d6618cd36ff041fdd5f13f9059cbb65f9c60
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-08-22 01:43:57 +0200

    Styling

--- template-parts/components/component-hero.php
+++ template-parts/components/component-hero.php
@@ -1,19 +1,37 @@
 <section class="hero">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner hero__slider">
-    <div class="carousel-item  hero__item active">
-      <div class="row g-0 d-flex mx-auto container">
-        <div class="col-sm-12 col-md-6 hero__bg">TEXT1</div>
-        <div class="col-sm-12 col-md-6 hero__img">
-          <img class="img" src="<?php echo get_template_directory_uri() . '/assets/slider1.jpeg'; ?>" alt="First slide">
+    <div class="carousel-item hero__item active">
+      <div class="row d-flex mx-auto container">
+        <div class="col-sm-12 col-md-6 d-flex flex-column hero__content">
+          <div class="d-flex flex-column hero__inner-content">
+          <h1 class="hero__title">Nowoczesna aranżacja Twojego ogrodu</h1>
+          <p class="hero__description">Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.</p>
+            <div class="d-flex justify-content-between hero__buttons">
+              <a href="#!" class="btn btn--primary">Skontaktuj się z nami</a>
+              <a href="#!" class="btn btn--secondary btn--arrow">Zobacz nasze realizacje</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 hero__image">
+          <img class="hero__img" src="<?php echo get_template_directory_uri() . '/assets/slider1.jpeg'; ?>" alt="First slide">
         </div>
       </div>
     </div>
-    <div class="carousel-item  hero__item">
-      <div class="row g-0 d-flex mx-auto container">
-        <div class="col-sm-12 col-md-6 hero__bg">TEXT2221</div>
-        <div class="col-sm-12 col-md-6 hero__img">
-          <img class="img" src="<?php echo get_template_directory_uri() . '/assets/slider1.jpeg'; ?>" alt="First slide">
+    <div class="carousel-item hero__item">
+      <div class="row d-flex mx-auto container">
+        <div class="col-sm-12 col-md-6 d-flex flex-column hero__content">
+          <div class="d-flex flex-column hero__inner-content">
+          <h1 class="hero__title">Nowoczesna aranżacja Twojego ogrodu</h1>
+          <p class="hero__description">Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.</p>
+            <div class="d-flex justify-content-between hero__buttons">
+              <a href="#!" class="btn btn--primary">Skontaktuj się z nami</a>
+              <a href="#!" class="btn btn--secondary btn--arrow">Zobacz nasze realizacje</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 hero__image">
+          <img class="hero__img" src="<?php echo get_template_directory_uri() . '/assets/slider1.jpeg'; ?>" alt="First slide">
         </div>
       </div>
     </div>
--- template-parts/global/header/template-header.php
+++ template-parts/global/header/template-header.php
@@ -1,5 +1,5 @@
-<header class="row g-0 header">
-  <div class="container d-flex justify-content-bewteen align-items-center">
+<header class="row header">
+  <div class="w-100 d-flex justify-content-bewteen align-items-center">
     <div class="header__logo col-lg-2">
       <img src="<?php echo get_template_directory_uri() . '/assets/logo.svg'; ?>" alt="<?php echo get_bloginfo( 'title' ); ?>">
     </div>
